c721f67f2739af26ca6b6c6c4f87c444fd887eb3
javierpro29 <andersonjavier2909@gmail.com>
2022-10-20 17:19:31 -0400

    validate operations buttons

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const display = document.querySelector("#display")
 const buttons = document.querySelectorAll("button")
 
-
 buttons.forEach((item) => {
     item.onclick = () => {
         if(item.id == "clear"){
@@ -10,15 +9,50 @@ buttons.forEach((item) => {
             let string = display.innerText.toString()
             display.innerText = string.substr(0, string.length - 1)  
         }else if(item.id == "iqual" && display.innerText != ""){
-            display.innerText = eval(display.innerText) 
+            try{
+                display.innerText = eval(display.innerText)
+            }catch(error){
+                display.innerText = "Syntax error"
+                setTimeout(() => {
+                    display.innerText = ""
+                }, 2000)
+            }
         }else if(item.id == "iqual" && display.innerText == ""){
             display.innerText = "null";
             setTimeout(() => (display.innerText = ""), 2000)
+        }else if(item.id == "00" && display.innerText == ""){
+            display.innerText = display.innerText   
+        }else if(item.id == "."){
+            let exist = display.innerText.includes(".")
+            if(!exist){
+                display.innerText += item.id 
+            }
+        }else if(item.id == "-"){
+            validateOperator(item.id)
+        }else if(item.id == "+"){
+            validateOperator(item.id)
+        }else if(item.id == "/"){
+            validateOperator(item.id)
+        }else if(item.id == "*"){
+            validateOperator(item.id)
         }else{
             display.innerText += item.id 
         }
     }
 }) 
+
+
+function validateOperator(item){
+    let exist = display.innerText.substr(-1)
+
+    if(item == "-" && exist != item){
+        display.innerText += item
+    }else if(display.innerText.length > 0 && exist != item){
+        display.innerText += item
+    }
+}
+
+
 
 
 const calculator = document.querySelector(".calculator")
